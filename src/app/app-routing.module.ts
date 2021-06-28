8a8ce8d99cc67e8089006a30ac6c3a08a85e357b
jeytoyota <jeytoyota@gmail.com>
2021-06-28 17:19:59 -0300

    rotas v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'login', pathMatch: 'full'},
+  {path:'login', component: LoginComponent},
+  {path:'cadastro', component: CadastroComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { LoginComponent } from './login/login.component';
+import { RodapeComponent } from './rodape/rodape.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
+    RodapeComponent
 
   ],
   imports: [
