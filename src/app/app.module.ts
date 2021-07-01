ad97c101f4c9d82f1a147d123e2e254f8041f104
viniciusmartins21 <viniciusmartins.12@hotmail.com>
2021-07-01 12:31:05 -0300

    Api teste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,7 @@ import { PaginainicialComponent } from './paginainicial/paginainicial.component'
 import { MenuComponent } from './menu/menu.component';
 import { LoginComponent } from './login/login.component';
 import { RodapeComponent } from './rodape/rodape.component';
+
 
 
 
@@ -24,6 +26,7 @@ import { RodapeComponent } from './rodape/rodape.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
