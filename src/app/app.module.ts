4ffc9de76bb49e59be14fc0c6eb6acdc8961438d
Jessica Toyota <59458333+jeytoyota@users.noreply.github.com>
2021-07-09 22:50:01 -0300

    Revert "Put usuario jessica"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,6 @@ import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
 import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
 import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
-import { UserEditComponent } from './edit/user-edit/user-edit.component';
 
 
 
@@ -47,8 +46,7 @@ import { UserEditComponent } from './edit/user-edit/user-edit.component';
     TemaEditComponent,
     TemaDeleteComponent,
     PostagemEditComponent,
-    PostagemDeleteComponent,
-    UserEditComponent
+    PostagemDeleteComponent
   ],
 
 
